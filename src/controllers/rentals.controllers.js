b70ee7764f7a8f0c9239d4fd272312102d9197a9
Lorena <blorena1321@gmail.com>
2023-05-14 19:46:54 -0300

    feat: finalRentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -13,13 +13,13 @@ export async function getRentals(req, res) {
         const finalRentals = rentals.rows.map(rec => {
             const { gamesName, customersName, customersId, gamesId, ...rest } = rec;
             return {
-              ...rest,
-              customer: { id: customersId, name: customersName },
-              game: { id: gamesId, name: gamesName }
+                ...rest,
+                customer: { id: customersId, name: customersName },
+                game: { id: gamesId, name: gamesName }
             };
-          });
-          
-          res.send(finalRentals);
+        });
+
+        res.send(finalRentals);
 
     } catch (err) {
         res.status(500).send(err.message);
@@ -30,23 +30,23 @@ export async function getRentals(req, res) {
 export async function postRentals(req, res) {
     const { customerId, gameId, daysRented } = req.body;
 
-    if(daysRented <=0){
+    if (daysRented <= 0) {
         return res.sendStatus(400);
     }
     try {
         const customerExists = await db.query(`SELECT * FROM customers WHERE id =$1;`, [customerId]);
-        if(customerExists.rows.length === 0){
-            return res.status(400).send("Cliente não existente.");   
+        if (customerExists.rows.length === 0) {
+            return res.status(400).send("Cliente não existente.");
         }
 
         const gameExists = await db.query(`SELECT * FROM games WHERE id=$1;`, [gameId]);
-        if(gameExists.rows.length === 0){
+        if (gameExists.rows.length === 0) {
             return res.status(400).send("Jogo não existente.")
         }
 
         const rentalsINProgress = await db.query(`SELECT COUNT (*) FROM rentals WHERE "gameId"=$1 AND "returnDate" IS NULL;`, [gameId]);
         const gameStockTotal = gameExists.rows[0].stockTotal;
-        if(rentalsINProgress.rows[0].count >= gameStockTotal){
+        if (rentalsINProgress.rows[0].count >= gameStockTotal) {
             return res.status(400).send("Não há jogos disponíveis para aluguel.");
         }
 
@@ -55,8 +55,8 @@ export async function postRentals(req, res) {
 
         await db.query(`
         INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee") 
-        VALUES ($1, $2, $3, $4, $5, NULL, NULL);`, 
-        [customerId, gameId, daysRented, rentDate, originalPrice]);
+        VALUES ($1, $2, $3, $4, $5, NULL, NULL);`,
+            [customerId, gameId, daysRented, rentDate, originalPrice]);
         res.sendStatus(201);
 
     } catch (err) {
@@ -65,24 +65,45 @@ export async function postRentals(req, res) {
 }
 
 export async function finalRentals(req, res) {
+    const { id } = req.params;
     try {
+        const rental = await db.query(`SELECT *FROM rentals WHERE id-$1`, [id]);
+        if (rental.rows.length === 0) {
+            return res.sendStatus(404);
+        }
+
+        const existingRental = rental.rows[0];
+        if (existingRental.returnDate) {
+            return res.sendStatus(400);
+        }
+
+        const returnDate = new Date();
+        const rentDate = existingRental.rentDate;
+        const daysRented = existingRental.daysRented;
+        const gamePricePerDay = existingRental.gamePricePerDay;
+
+        const delayInDays = Math.ceil((returnDate - rentDate) / (1000 * 60 * 60 * 24)) - daysRented;
+        const delayFee = Math.max(0, delayInDays) * gamePricePerDay;
+
+        await db.query(`UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3`, [returnDate, delayFee, id]);
+        return rentDate.sendStatus(200);
 
     } catch (err) {
-
+        res.status(500).send(err.message);
     }
 
 }
 
 export async function deleteRentals(req, res) {
-    const {id} = req.params;
+    const { id } = req.params;
     try {
-        const idExists = await db.query(`SELECT * FROM rentals WHERE id=$1;`,[id]);
-        if(idExists.rows.length === 0){
+        const idExists = await db.query(`SELECT * FROM rentals WHERE id=$1;`, [id]);
+        if (idExists.rows.length === 0) {
             return res.status(404).send("Id não existe.")
         }
 
         const existingRental = rental.rows[0];
-        if(existingRental.returnDate){
+        if (existingRental.returnDate) {
             return res.sendStatus(400);
         }
 
