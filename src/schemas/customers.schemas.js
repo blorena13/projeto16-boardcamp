95465554ccca9370893c2623fc0c3c2207ba169c
Lorena <blorena1321@gmail.com>
2023-05-14 20:27:29 -0300

    fix: update rentals

--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -4,5 +4,5 @@ export const customersSchema = Joi.object({
     name: Joi.string().empty().required(),
     phone: Joi.string().min(10).max(11).pattern(/^[0-9]+$/),
     cpf: Joi.string().min(11).max(11).pattern(/^[0-9]+$/),
-    birthday: Joi.string().pattern(/^[0-9]{4}[-][0-9]{2}[-][0-9]{2}$/).required(),
+    birthday: Joi.date().raw().required(),
 })
